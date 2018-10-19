0f4b0604930ccf092ecfc47e4c1867e8f60ab85f
Ilya Gorev <core.autocrlf>
2018-10-19 19:51:52 +0700

    Добавляет gulp-plumber  (не прерывает сборку на ошибках)

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
 
 gulp.task("style", function () {
   return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest("source/css"))
 });
