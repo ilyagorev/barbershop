421560983e7f42644cabbaaa427078495d72e85a
Ilya Gorev <core.autocrlf>
2018-10-19 20:04:37 +0700

    Добавляет в сборку PostCSS  с autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 "use strict";
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
+var gulp = require("gulp"); // gulp таск-раннер
+var sass = require("gulp-sass"); //SASS препроцессор
+var plumber = require("gulp-plumber"); //Сообщает об ошибках, не прерывая сборку
+/*POSTCSS с плагинов для автопрефикса*/
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
 
 gulp.task("style", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
+    .pipe(postcss([
+        autoprefixer()
+    ]))
     .pipe(gulp.dest("source/css"))
 });
